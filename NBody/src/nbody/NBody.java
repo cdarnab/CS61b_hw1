eceee51567e4e2689df13a2966307978f9c79e09
troy <sabotuer99@gmail.com>
2015-05-10 16:26:22 -0600

    Initial commit of homework 1

---
+++ NBody/src/nbody/NBody.java
@@ -0,0 +1,7 @@
+package nbody;
+
+public class NBody {
+	public static Planet getPlanet(In in){
+		return new Planet(0, 0, 0, 0, 0, "");
+	}
+}
---
+++ NBody/src/nbody/RunAllTests.java
@@ -0,0 +1,12 @@
+package nbody;
+
+public class RunAllTests {
+    public static void main(String[] args) {
+        TestPlanetConstructor.main(args);
+        TestCalcDistance.main(args);
+        TestCalcPairwiseForce.main(args);
+        TestCalcPairwiseForceXY.main(args);
+        TestSetNetForce.main(args);
+    	//TestUpdate.main(args);
+    }
+}
